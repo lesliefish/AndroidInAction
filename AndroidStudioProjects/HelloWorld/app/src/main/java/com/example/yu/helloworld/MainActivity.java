1e7722655eed4eb356866eb57d0dbd1c4ddc7b48
leiyu <yuleics@outlook.com>
2018-10-25 23:10:30 +0800

    隐藏标题栏

--- AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/yu/helloworld/MainActivity.java
+++ AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/yu/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.RadioButton;
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         mButton = findViewById(R.id.button);
         mButton.setOnClickListener(new Button.OnClickListener(){
