5569b1f63ed84a36331bd682f33c749f8ba851fa
leiyu <yuleics@outlook.com>
2018-11-10 23:47:02 +0800

    activity life circle

---
+++ AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/yu/activityAndIntents/ActivityLifeCircleActivity.java
@@ -0,0 +1,53 @@
+package com.example.yu.activityAndIntents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.yu.helloworld.R;
+
+public class ActivityLifeCircleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_circle);
+        Log.d("lifecycle","onCreate invoked");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle","onResume invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle","onCreate invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onPause invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
+    }
+}
---
+++ AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/yu/helloworld/ActivityAndIntentActivity.java
@@ -0,0 +1,25 @@
+package com.example.yu.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityAndIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_and_intent);
+
+        Button activityLifeCircleBtn = findViewById(R.id.lifeCircleBtn);
+        activityLifeCircleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ActivityAndIntentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
